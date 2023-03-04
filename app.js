78e52fb727916bbecac64792bc6db420744407d1
it21038914 <it21038914@my.sliit.lk>
2023-03-04 09:44:27 +0530

    Added USing module

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-const fs = require('fs');
-fs.readFile('file.txt', 'utf8', function (err, data) {
+const fs = require("fs");
+fs.readFile("file.txt", "utf8", function (err, data) {
   if (err) throw err;
   console.log(data);
 });
 
-const fs = require('fs');
-fs.writeFile('file.txt', 'Hello World!', function (err) {
+fs.writeFile("file.txt", "Hello World!", function (err) {
   if (err) throw err;
-  console.log('File saved!');
+  console.log("File saved!");
 });
+
+//creating web Server
+const http = require("http");
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("Hello World!");
+    res.end();
+  })
+  .listen(8080);
+
+//using a module
+const myModule = require("./my-module.js");
+console.log(myModule.myFunction());
