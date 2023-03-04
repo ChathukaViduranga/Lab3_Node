a89660ad369ebcd592f07875ae2100c26b3875fe
it21038914 <it21038914@my.sliit.lk>
2023-03-04 09:24:00 +0530

    Added Creating web Serever

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const fs = require("fs");
 fs.readFile("file.txt", "utf8", function (err, data) {
   if (err) throw err;
@@ -11,4 +10,12 @@ fs.writeFile("file.txt", "Hello World!", function (err) {
   console.log("File saved!");
 });
 
-
+//creating web Server
+const http = require("http");
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("Hello World!");
+    res.end();
+  })
+  .listen(8080);
