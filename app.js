04e2f214bbc65a58de406c5fcd63814bd75ac228
Chathuka Viduranga <Chathuka.viduranga.bc@gmail.com>
2023-03-04 09:37:41 +0530

    error resolved

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
-
-const fs = require("fs");
-fs.readFile("file.txt", "utf8", function (err, data) {
-
+const fs = require('fs');
 
 fs.readFile('file.txt', 'utf8', function (err, data) {
-
   if (err) throw err;
   console.log(data);
 });
-
 
 fs.writeFile('file.txt', 'Hello World!', function (err) {
   if (err) throw err;
   console.log('File saved!');
 });
 
-
 //creating web Server
-const http = require("http");
+const http = require('http');
 http
   .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("Hello World!");
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('Hello World!');
     res.end();
   })
   .listen(8080);
-
