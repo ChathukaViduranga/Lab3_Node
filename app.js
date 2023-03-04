feec1c9162bb63717f25fc01e1240f091961c141
T.K.G.dharamsiri <90247541+it21038914@users.noreply.github.com>
2023-03-04 09:52:18 +0530

    Added USing module (#6)

--- app.js
+++ app.js
@@ -1,26 +1,48 @@
+
+const fs = require("fs");
+fs.readFile("file.txt", "utf8", function (err, data) {
+
 const fs = require('fs');
 
 fs.readFile('file.txt', 'utf8', function (err, data) {
+
   if (err) throw err;
   console.log(data);
 });
 
+
+fs.writeFile("file.txt", "Hello World!", function (err) {
+
 fs.writeFile('file.txt', 'Hello World!', function (err) {
+
   if (err) throw err;
-  console.log('File saved!');
+  console.log("File saved!");
 });
 
 //creating web Server
+
+const http = require("http");
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("Hello World!");
+
 const http = require('http');
 http
   .createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.write('Hello World!');
+
     res.end();
   })
   .listen(8080);
 
-const https = require('https');
+
+//using a module
+const myModule = require("./my-module.js");
+console.log(myModule.myFunction());
+
+
 https
   .get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
     let data = '';
@@ -34,3 +56,4 @@ https
   .on('error', (err) => {
     console.log('Error: ' + err.message);
   });
+
